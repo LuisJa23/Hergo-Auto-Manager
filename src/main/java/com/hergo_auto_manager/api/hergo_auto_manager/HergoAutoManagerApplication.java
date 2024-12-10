afddece43b7db77cea43133f8cb8009803999714
Luis Javier Lopez Galindo <javierlink22@gmail.com>
2024-12-09 21:25:11 -0500

    Firts Commit

---
+++ src/main/java/com/hergo_auto_manager/api/hergo_auto_manager/HergoAutoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hergo_auto_manager.api.hergo_auto_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HergoAutoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HergoAutoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hergo_auto_manager/api/hergo_auto_manager/HergoAutoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hergo_auto_manager.api.hergo_auto_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HergoAutoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
